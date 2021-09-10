5cbb0f4f0a6164fa3307405eec83e2c2fb87381a
Zohaib Butt <zohaib.butt@pearlgroup.no>
2021-09-10 21:28:01 +0200

    Added matchmaking ui and session management. need refactoring and clean up

--- server/server.js
+++ server/server.js
@@ -14,20 +14,57 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
-let waitingPlayer = null;
-
+let sessions = [];
 // Connection is made by a player.
 io.on("connection", (sock) => {
-	console.log("someone is connected");
+	sock.on("Create Session", (newSession) => {
+		console.log("New session " + newSession.id + " created!");
 
-	if(waitingPlayer && sock != waitingPlayer) {
-		// start a game
-		new TicTacToe(waitingPlayer, sock);
-		waitingPlayer = null;
-	} else {
-		waitingPlayer = sock;
-		waitingPlayer.emit("message", "Searching for a player!");
-	}
+		newSession.player1 = sock.id;
+		newSession.player2 = null;
+		sessions.push(newSession);
+		io.emit("All Sessions", sessions)
+	});
+	sock.on("Join Session", (sessionID) => {
+		console.log("Joining Session " + sessionID);
+
+		var filtered = sessions.filter(session => session.id === sessionID);
+		if (filtered.length == 1) {
+			var i = sessions.indexOf(filtered[0]);
+			if (sessions[i].player2 == null && sessions[i].player1 != sessions[i].player2) {
+				sessions[i].player2 = sock.id;
+				new TicTacToe(io.sockets.sockets.get(sessions[i].player1), sock);
+				io.emit("All Sessions", sessions);
+			}
+		}
+	});
+	sock.on("Get All Session", () => {
+		io.emit("All Sessions", sessions);
+	})
+	sock.on("disconnect", function() {
+		console.log("Socket " + sock.id + "got disconnect!");
+
+		var session = null;
+		for (var i = 0; i < sessions.length; i++) {
+			if (sessions[i].player1 === sock.id) {
+				session = sessions[i];
+			} else if (sessions[i].player2 === sock.id) {
+				session = sessions[i];
+			}
+		}
+		if (session != null && session.player1 == sock.id && io.sockets.sockets.get(session.player2)) {
+			io.sockets.sockets.get(session.player2).emit("refresh");
+			io.sockets.sockets.get(session.player2).disconnect();
+		} else if (session != null && session.player2 == sock.id && io.sockets.sockets.get(session.player1)) {
+			io.sockets.sockets.get(session.player1).emit("refresh");
+			io.sockets.sockets.get(session.player1).disconnect();
+		}
+
+		var i = sessions.indexOf(session);
+		sessions.splice(i, 1);
+		io.emit("All Sessions", sessions);
+		
+	});
 });
 
 server.on("error", (err) => {
